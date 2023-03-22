2af354ec5d61be97b49a10032264b6ef5752e42c
cornishjed <judgejed@gmail.com>
2023-03-22 17:01:47 +0000

    Fixed grid size, boxes resize to fit

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let boxCount = 0;
 
-const button = document.querySelector("#getCount");
+const button1 = document.querySelector("#getCount");
+const button2 = document.querySelector("#reset");
+const gridbox = document.getElementById("gridbox");
 
-button.addEventListener("click", function () {
+button1.addEventListener("click", function () {
   boxCount = prompt("Enter grid size");
-  console.log(boxCount);
-
-  const gridbox = document.getElementById("gridbox");
 
   gridbox.replaceChildren();
 
   // create grid
   for (let y = 0; y < boxCount; y++) {
     const boxY = document.createElement("div");
+    boxY.classList = "boxY";
     gridbox.appendChild(boxY);
     for (let x = 0; x < boxCount; x++) {
       const boxX = document.createElement("div");
@@ -24,6 +24,15 @@ button.addEventListener("click", function () {
         boxX.style.backgroundColor = randColor;
       });
       boxY.appendChild(boxX);
+    }
+  }
+});
+
+button2.addEventListener("click", function () {
+  const boxX = document.querySelector(".box");
+  for (let y = 0; y < boxCount; y++) {
+    for (let x = 0; x < boxCount; x++) {
+      boxX.style.backgroundColor = "pink";
     }
   }
 });
