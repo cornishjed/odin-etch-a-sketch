858c3c37571c3aff48ed8daf7d0c0a0f68129c55
cornishjed <judgejed@gmail.com>
2023-03-23 09:33:55 +0000

    Exercise complete. Added grid limit, reset function and refined styling

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 let boxCount = 0;
-
+const gridbox = document.getElementById("gridbox");
 const button1 = document.querySelector("#getCount");
 const button2 = document.querySelector("#reset");
-const gridbox = document.getElementById("gridbox");
 
-button1.addEventListener("click", function () {
-  boxCount = prompt("Enter grid size");
-
+function createGrid() {
   gridbox.replaceChildren();
-
-  // create grid
   for (let y = 0; y < boxCount; y++) {
     const boxY = document.createElement("div");
     boxY.classList = "boxY";
@@ -17,6 +12,7 @@ button1.addEventListener("click", function () {
     for (let x = 0; x < boxCount; x++) {
       const boxX = document.createElement("div");
       boxX.classList = "box";
+      boxX.style.backgroundColor = "white";
       boxX.addEventListener("mouseover", function (e) {
         let randColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
           Math.random() * 256
@@ -26,13 +22,17 @@ button1.addEventListener("click", function () {
       boxY.appendChild(boxX);
     }
   }
+}
+
+button1.addEventListener("click", function () {
+  boxCount = prompt("Enter grid size");
+  if (boxCount > 100) {
+    alert("Limit exceeded. Maximum allowed is 100.")
+    boxCount = 100;
+  }
+  createGrid();
 });
 
 button2.addEventListener("click", function () {
-  const boxX = document.querySelector(".box");
-  for (let y = 0; y < boxCount; y++) {
-    for (let x = 0; x < boxCount; x++) {
-      boxX.style.backgroundColor = "pink";
-    }
-  }
+  createGrid();
 });
